f9d48abab3191d58820166f403bf7d071b05181f
Aditya Dixit <dixitaditya20@gmail.com>
2021-09-01 18:46:59 +0530

    MicroServices Login Page

---
+++ src/test/java/com/springcustomlogin/LoginApplication.java
@@ -0,0 +1,13 @@
+package com.springcustomlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
